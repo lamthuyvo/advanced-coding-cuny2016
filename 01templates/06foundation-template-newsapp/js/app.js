d0c8d156deefa3207d53cfcc466daa7f848d2464
lamthuyvo <lamthuyvo@gmail.com>
2016-10-13 15:11:58 -0400

    added new semantic ui files

---
+++ 01templates/06foundation-template-newsapp/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	// this is where your pseudocode and jquery goes!
+	// make a selection option for each row in your data set, i.e. make a dropdown menu
+	_.each(foodWasteData, function(value, index){
+		$('.dropdown').append('<option class="item" data-value="'+value.food_group+'">'+value.food_group+'</option>')
+	})
+	
+	// activate semantic-ui
+	$('.dropdown').dropdown({
+		fullTextSearch: true,
+	});
+
+
+
+});
