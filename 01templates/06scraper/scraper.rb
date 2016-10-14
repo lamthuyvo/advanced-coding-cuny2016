4f2f69e76591a0bc6857c54d5c98620772d260da
lamthuyvo <lamthuyvo@gmail.com>
2016-10-14 14:24:24 -0400

    tweaked scraper

--- 01templates/06scraper/scraper.rb
+++ 01templates/06scraper/scraper.rb
@@ -1,25 +1,26 @@
-# nokogiri
+# 'require' is like linking to a library; this is where you preload all the Ruby 'gems' — pre-written Ruby code with methods you can use — before you start writing your scraper
+
+# nokogiri is a scraping gem
 require 'nokogiri'
-
+# open-uri allows you to open html pages and web sites
 require 'open-uri'
-
+# this allows you to open and write comma-separated values files 
 require 'csv'
 
 # open html
 url ="https://en.wikipedia.org/wiki/List_of_animated_short_films"
 
-# loac URL into Nokogiri for parscing and downloading page into csv
+# load URL into Nokogiri for parsing and downloading page into csv
 data = Nokogiri::HTML(open(url))
 
 # target data using css selectors
 rows = data.css('tr')
 
 # open a csv file for data to be dumped into
-csv = CSV.open("dates.csv", "wb")
+csv = CSV.open("films.csv", "wb")
 
 
 # method for rows and items
-
 rows.each do |row|
 	# creates an array
 	array = []
