411b03957d8a4fa1888fd87057c9ce7eec23f990
lamthuyvo <lamthuyvo@gmail.com>
2016-11-18 18:19:49 -0500

    took out code

--- 01templates/09d3-draw/js/app.js
+++ 01templates/09d3-draw/js/app.js
@@ -1,27 +1,19 @@
 $(document).ready(function(){
 
 	// set the specs
-	var width = 900;
-	var height = 450;
-
+	
 	// create SVG element
-	var svg = d3.select("#chart")
-	            .append("svg")
-	            .attr("width", width)
-	            .attr("height", height);
+
 
 	
 	// loading data
-	var data = d3.csv("js/data.csv", function(error, data) {
+	
 
 		// define the newly transformed data set (from csv into a data object)
-		foodWasteData = data; 
+		
 
 		// make a rectangle for each data point, giving each rectangle an x-coordinate, a y-coordinate, a width and a height (optional, give it a fill)
 	   	
-
-	});
-
 
 	
 
